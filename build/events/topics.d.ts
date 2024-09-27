7e07f7b1c30c9f456f47077026a203838ee8eab1
tmd0702 <mduc017@gmail.com>
2024-09-27 13:54:03 +0700

    feat [common]: npm common package for reusing between services;

--- build/events/topics.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum Topics {
-    UserCreated = "user.created",
-    UserUpdated = "user.updated",
-    EmailVerificationNeeded = "verification.email",
-    SMSVerificationNeeded = "verification.sms"
-}
